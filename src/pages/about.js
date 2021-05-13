11ee1d1b850a64c85692a2ecf3768e9f2a2ce71f
Ishan Gupta <50520498+ishanguptaofficial@users.noreply.github.com>
2021-05-13 18:42:59 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -24,16 +24,7 @@ const AboutPage = ({data}) => (
   <Teamphotosection />  
   <InfoBlock heading="About our vision"/>
   
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
-  <ins class="adsbygoogle"
-       style="display:block"
-       data-ad-client="ca-pub-2137854737828188"
-       data-ad-slot="8237762020"
-       data-ad-format="auto"
-       data-full-width-responsive="true" />
-  <script>
-       (adsbygoogle = window.adsbygoogle || []).push({});
-  </script>
+
   </Layout>
 )
 
